35b4a05a7599bc2a97ff313e961edc007a25b2bb
mehdiig2120 <mehdigolestanii27@gmail.com>
2026-02-16 20:41:50 +0330

    commit3

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ Docstring for main
 starting in 16 feb 
 '''
 import emoji
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
 import tqdm
 import time 
-import lxml
 
-result = requests.get("https://bitarz.net/prices/")    #get information from wallex website 
-soup = BeautifulSoup(result.text,"html.parser")
+driver = webdriver.Chrome()
+driver.get("https://wallex.ir/price/btc")    #get information from wallex website 
+
+from tqdm import tqdm
+for i in tqdm(range(100)):
+    time.sleep(0.1)
+
+soup = BeautifulSoup(driver.page_source,"html.parser")
+print(soup.prettify())
+driver.quit()
 print(soup)
 
