a95be180ddc433b67257d2f381245596d3db945a
mehdiig2120 <mehdigolestanii27@gmail.com>
2026-02-16 19:07:08 +0330

    commit 2

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 Docstring for main
 starting in 16 feb 
 '''
+import emoji
+import requests
+from bs4 import BeautifulSoup
+import tqdm
+import time 
+import lxml
+
+result = requests.get("https://bitarz.net/prices/")    #get information from wallex website 
+soup = BeautifulSoup(result.text,"html.parser")
+print(soup)
+
