8929f84e6f6c4c2051b3fa8956fdeb1d220291db
mehdiig2120 <mehdigolestanii27@gmail.com>
2026-02-18 15:27:51 +0330

    commit 4

--- main.py
+++ main.py
@@ -3,20 +3,40 @@ Docstring for main
 starting in 16 feb 
 '''
 import emoji
-from selenium import webdriver
+import requests
 from bs4 import BeautifulSoup
 import tqdm
 import time 
-
-driver = webdriver.Chrome()
-driver.get("https://wallex.ir/price/btc")    #get information from wallex website 
-
+import re
 from tqdm import tqdm
-for i in tqdm(range(100)):
-    time.sleep(0.1)
 
-soup = BeautifulSoup(driver.page_source,"html.parser")
-print(soup.prettify())
-driver.quit()
-print(soup)
+url = "https://wallex.ir/price/btc"         # i used from wallex website 
+answer = input("you want bitcoin (y/n) ?")
+if answer == "y" :
+    while True :
+        try :
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text,"html.parser")
+            text = soup.get_text()
 
+            number = re.search(r"([0-9]{1,3}(?:,[0-9]{3})+)", text)
+            if number :
+                price = number.group(1)
+                for i in tqdm(range(100)):
+                    time.sleep(0.03)
+
+
+                print(f'price of bitcoin : {price}')
+                answer2 = input("show again ? (y/n)")
+                if answer2 == "y":
+                    pass
+                else:
+                    print("ok! have good day")
+                    break
+            else :
+                print("i can not find any thing")
+        except Exception as e :
+            print(f'error : {e}')
+        time.sleep(5)
+else : 
+    print("ok! have good day ")
