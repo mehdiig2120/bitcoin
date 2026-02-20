c39d9ab37db9ea661778a594c63941f7b463b93e
mehdiig2120 <mehdigolestanii27@gmail.com>
2026-02-20 19:05:53 +0330

    commit 5

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 '''
 Docstring for main
 starting in 16 feb 
+(web scraping + api + nested loop )
+just for tranning
+
 '''
 import emoji
 import requests
@@ -11,7 +14,7 @@ import re
 from tqdm import tqdm
 
 url = "https://wallex.ir/price/btc"         # i used from wallex website 
-answer = input("you want bitcoin (y/n) ?")
+answer = input("you want price of bitcoin (y/n) \U0001F600 ?")
 if answer == "y" :
     while True :
         try :
@@ -26,12 +29,12 @@ if answer == "y" :
                     time.sleep(0.03)
 
 
-                print(f'price of bitcoin : {price}')
+                print(f'price of bitcoin : {price} Toman ')
                 answer2 = input("show again ? (y/n)")
                 if answer2 == "y":
                     pass
                 else:
-                    print("ok! have good day")
+                    print(emoji.emojize("ok! have good day :red_heart:", variant="emoji_type"))
                     break
             else :
                 print("i can not find any thing")
@@ -39,4 +42,4 @@ if answer == "y" :
             print(f'error : {e}')
         time.sleep(5)
 else : 
-    print("ok! have good day ")
+    print(emoji.emojize("ok! have good day :red_heart:", variant="emoji_type"))
